bf24354ff542877475aa401e2c9a115b4a88f1c2
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2020-07-14 22:33:23 -0500

    Updates

---
+++ build/precache-manifest.9d7e5f0ebaff52199e7d2e4285ed1c0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b107b1dbfaf0da98c2296893eb2431",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "bab640d0d8b97de4a835",
+    "url": "/gallery/static/css/2.3a7595ce.chunk.css"
+  },
+  {
+    "revision": "826bafb5942bbe4ae09b",
+    "url": "/gallery/static/css/main.ea9dc465.chunk.css"
+  },
+  {
+    "revision": "bab640d0d8b97de4a835",
+    "url": "/gallery/static/js/2.ce92328c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gallery/static/js/2.ce92328c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "826bafb5942bbe4ae09b",
+    "url": "/gallery/static/js/main.02739527.chunk.js"
+  },
+  {
+    "revision": "7dd180262069cbb141c9",
+    "url": "/gallery/static/js/runtime-main.27649e68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gallery/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ precache-manifest.4b9e3808749c0c4660e87e13d7dcfc8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffdd91fe6cb0bce730a9020f2071b026",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "9088f261447835aac0be",
+    "url": "/gallery/static/css/2.3a7595ce.chunk.css"
+  },
+  {
+    "revision": "bf6ab352e97b8f43f64a",
+    "url": "/gallery/static/css/main.d9faf013.chunk.css"
+  },
+  {
+    "revision": "9088f261447835aac0be",
+    "url": "/gallery/static/js/2.3e2d19d5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gallery/static/js/2.3e2d19d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6ab352e97b8f43f64a",
+    "url": "/gallery/static/js/main.6e59192c.chunk.js"
+  },
+  {
+    "revision": "7dd180262069cbb141c9",
+    "url": "/gallery/static/js/runtime-main.27649e68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gallery/static/media/logo.5d5d9eef.svg"
+  }
+]);
